b1806d2c8cedfdc20b7e790db09432faea8427a1
hp <hp@KOBBYFLEX>
2024-06-29 12:32:37 +0000

    completed GradeCalculator

---
+++ Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade between 0 and 100: ");
+            string input = Console.ReadLine();
+            int grade;
+
+            if (int.TryParse(input, out grade) && grade >= 0 && grade <= 100)
+            {
+                string letterGrade = GetLetterGrade(grade);
+                Console.WriteLine("The corresponding letter grade is: " + letterGrade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static string GetLetterGrade(int grade)
+        {
+            if (grade >= 90) return "A";
+            else if (grade >= 80) return "B";
+            else if (grade >= 70) return "C";
+            else if (grade >= 60) return "D";
+            else return "F";
+        }
+    }
+
+}
+    
+
